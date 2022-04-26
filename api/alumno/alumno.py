951f0ff0b26671a2a3784883dfdee19b76e52469
fabrizzioalco <fabrizzio166@gmail.com>
2022-04-25 20:04:43 -0500

    Adding validations and finish methods

--- api/alumno/alumno.py
+++ api/alumno/alumno.py
@@ -7,17 +7,18 @@ alumnos=[]
 @alumno_bp.route("/alumno", methods=["POST"])
 def create_alumno():
 	alumno = request.get_json()
-	# print(alumno)
-
 	#TODO: Create validation. And create from model
+	if alumno["id"] in alumnos:
+		return jsonify({"error": "alumno already exists"}), 400
+
 	alumnos.append(alumno)
 	print(alumnos)
-	return alumno
+	return alumno, 200
 
 #GET
 @alumno_bp.route("/alumnos", methods=["GET"])
 def get_alumnos():
-	return jsonify(alumnos)
+	return jsonify(alumnos), 200
 
 @alumno_bp.route("/alumno/<int:id>", methods=["PUT", "GET"])
 def get_alumno(id):
@@ -27,15 +28,21 @@ def get_alumno(id):
 	alumno_id = request.get_json().get('id')
 
 	if request.method == "PUT":
-		return "Actualizar alumno_bp"
+		for student in alumnos:
+			if student["id"] == alumno_id:
+				student["nombre"] = request.get_json().get('nombre')
+				student["apellido"] = request.get_json().get('apellido')
+				student["matricula"] = request.get_json().get('matricula')
+				student["promedio"] = request.get_json().get('promedio')
+				return jsonify(student), 200
 
 	for student in alumnos: 
 		if student["id"] == alumno_id:
-			return student
+			return student, 200
 		else:
-			return "No existe alumno_bp con id: {}".format(alumno_id)
+			return "No existe alumno_bp con id: {}".format(alumno_id), 404
 
-@alumno_bp.route("/alumnos/<int:id>", methods=["DELETE"])
+@alumno_bp.route("/alumno/<int:id>", methods=["DELETE"])
 def delete_alumno(id):
 	"""
 	Elimina un alumno_bp
@@ -45,7 +52,8 @@ def delete_alumno(id):
 
 	for student in alumnos: 
 		if student["id"] == alumno_id:
+
 			alumnos.remove(student)
+			return "Alumno eliminado", 200
 
-
-	return "Alumno eliminado"
+	return "No existe alumno_bp con id: {}".format(alumno_id), 404
